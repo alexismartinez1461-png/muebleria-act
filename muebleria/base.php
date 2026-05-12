0fc0495cc3d410558653ed56ecfb3ab630d5d83f
franciscourzua2722 <francisco.urzua2722@alumnos.udg.mx>
2026-05-12 17:43:16 -0600

    base.php

--- muebleria/base.php
+++ muebleria/base.php
@@ -10,7 +10,6 @@ $nav_links = [
     'contacto.php' => 'Contacto',
 ];
 
-$categorias_footer = ['Cocina', 'Oficina', 'Jardín', 'Cochera', 'Dormitorios'];
 $sobre_nosotros    = ['Nuestra Historia', 'Misión, Visión y Valores', 'Carreras', 'Política de Privacidad', 'Términos del Servicio'];
 $soporte           = ['Preguntas Frecuentes', 'Ayuda en línea', 'Confianza y Seguridad'];
 ?>
